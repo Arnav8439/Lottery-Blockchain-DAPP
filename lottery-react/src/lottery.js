226ad2891a9ff9c84d9ab338a7d0faa96dd55549
Arnav Aggarwal <arnavaggarwal8755@gmail.com>
2020-06-25 11:34:41 +0530

    Lottery DAPP

---
+++ lottery-react/src/lottery.js
@@ -0,0 +1,18 @@
+import web3 from './web3';
+
+const address = '0x2656509f1199553e2dea402461E1a0073e16dD8f';
+
+const abi = 
+[{"constant":true,"inputs":[],"name":"manager","outputs":[{"name":"","type":"address"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[],"name":"pickWinner","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":false,"inputs":[],"name":"enter","outputs":[],"payable":true,"stateMutability":"payable","type":"function"},{"constant":true,"inputs":[],"name":"getplayers","outputs":[{"name":"","type":"address[]"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[{"name":"","type":"uint256"}],"name":"players","outputs":[{"name":"","type":"address"}],"payable":false,"stateMutability":"view","type":"function"},{"inputs":[],"payable":false,"stateMutability":"nonpayable","type":"constructor"}];
+
+
+export default new web3.eth.Contract(abi, address);
+
+
+
+
+
+
+
+
+
